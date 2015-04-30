05824c3eb3f814729245c7a806a8ccc24e02a23e
luyijun <luyijun999@gmail.com>
2015-05-01 03:01:55 +0800

    Modified webclient, put webclient functions into a namespace and changed webclient UI.

--- muddery/web/webclient/default/static/webclient/js/muddery_ajax_webclient.js
+++ muddery/web/webclient/default/static/webclient/js/muddery_ajax_webclient.js
@@ -16,6 +16,19 @@ function msg_display(type, msg){
         var data = {"sys": msg};
     }
     
-    display_data(data);
+    webclient.displayData(data);
 }
 
+
+function sendCommand(command) {
+    sendRequest(command);
+}
+
+function sendRequest(request) {
+    // relays data from client to Evennia.
+    webclient_input(request);
+}
+
+function webclient_set_sizes() {
+    webclient.doSetSizes();
+}
--- muddery/web/webclient/default/static/webclient/js/muddery_websocket_webclient.js
+++ muddery/web/webclient/default/static/webclient/js/muddery_websocket_webclient.js
@@ -1,28 +1,42 @@
 
-function doShow(type, msg){
-    if (type == "out"){
+function doShow(type, msg) {
+    if (type == "out") {
         try {
             var data = JSON.parse(msg);
         }
-        catch(err){
+        catch(err) {
             // not JSON packed - a normal text message
             var data = {"out": msg};
         }
     }
-    else if (type == "err"){
+    else if (type == "err") {
         var data = {"err": msg};
     }
-    else if (type == "sys"){
+    else if (type == "sys") {
         var data = {"sys": msg};
     }
-    else if (type == "debug"){
+    else if (type == "debug") {
         var data = {"debug": msg};
     }
                 
-    display_data(data);
+    webclient.displayData(data);
 }
 
-function doPrompt(type, msg){
+function doPrompt(type, msg) {
     var data = {"prompt": msg};
-    display_data(data);
+    webclient.displayData(data);
 }
+
+function sendCommand(command) {
+    sendRequest('CMD ' + command);
+}
+
+function sendRequest(request) {
+    // relays data from client to Evennia.
+    websocket.send(request);
+}
+
+function doSetSizes() {
+    webclient.doSetSizes();
+}
+
