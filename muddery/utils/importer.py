2c00f1b02a6a0352371983c4e591cc0de1a57bc7
luyijun <luyijun999@gmail.com>
2015-11-07 02:40:12 +0800

    Support excel format data files.

--- muddery/utils/importer.py
+++ muddery/utils/importer.py
@@ -4,27 +4,46 @@ This module imports data from files to db.
 
 import os
 import glob
-import csv
 from django.db import models
 from django.db.models.loading import get_model
 from django.conf import settings
 from evennia.utils import logger
 from muddery.utils.exception import MudderyError
+from muddery.utils import readers
 
 
-def import_csv(file_name, model_name, clear=True):
+def import_file(file_name, model_name, widecard=True, clear=True):
     """
-    Import data from a csv file to the db model
+    Import data from a data file to the db model
 
     Args:
-        file_name: (string) CSV file's name.
+        file_name: (string) file's name
         model_name: (string) db model's name.
+        widecard: (bool) add widecard or not.
         clear: (boolean) clear old data or not.
     """
-    try:
-        # load file
-        csvfile = open(file_name, 'r')
-        reader = csv.reader(csvfile)
+    # get file list
+    if widecard:
+        file_names = glob.glob(file_name + ".*")
+    else:
+        file_names = [file_name]
+
+    imported = False
+    for file_name in file_names:
+        # get file's extension name
+        ext_name = os.path.splitext(file_name)[1].lower()
+
+        reader = None
+        if ext_name == ".csv":
+            reader = readers.csv_reader(file_name)
+        elif ext_name == ".xls" or ext_name == ".xlsx":
+            reader = readers.xls_reader(file_name)
+
+        if not reader:
+            # does support this file type, read next one.
+            continue
+
+        imported = True
 
         # get model
         model_obj = get_model(settings.WORLD_DATA_APP, model_name)
@@ -33,121 +52,121 @@ def import_csv(file_name, model_name, clear=True):
             # clear old data
             model_obj.objects.all().delete()
 
-        # read title
-        title = reader.next()
+        try:
+            # read title
+            title = reader.next()
 
-        # get field types
-        # type = 0    means common field
-        # type = 1    means Boolean field
-        # type = 2    means Integer field
-        # type = 3    means Float field
-        # type = 4    means ForeignKey field
-        # type = 5    means ManyToManyField field, not support
-        # type = -1   means field does not exist
+            # get field types
+            # type = 0    means common field
+            # type = 1    means Boolean field
+            # type = 2    means Integer field
+            # type = 3    means Float field
+            # type = 4    means ForeignKey field
+            # type = 5    means ManyToManyField field, not support
+            # type = -1   means field does not exist
 
-        field_types = []
-        related_fields = []
-        for field_name in title:
-            field_type = -1
-            related_field = 0
+            field_types = []
+            related_fields = []
+            for field_name in title:
+                field_type = -1
+                related_field = 0
 
-            try:
-                # get field info
-                field = model_obj._meta.get_field(field_name)
+                try:
+                    # get field info
+                    field = model_obj._meta.get_field(field_name)
 
-                if isinstance(field, models.BooleanField):
-                    field_type = 1
-                elif isinstance(field, models.IntegerField):
-                    field_type = 2
-                elif isinstance(field, models.FloatField):
-                    field_type = 3
-                elif isinstance(field, models.ForeignKey):
-                    field_type = 4
-                    related_field = field.related_field
-                elif isinstance(field, models.ManyToManyField):
-                    field_type = 5
-                else:
-                    field_type = 0
-            except Exception, e:
-                logger.log_errmsg("Field error: %s" % e)
+                    if isinstance(field, models.BooleanField):
+                        field_type = 1
+                    elif isinstance(field, models.IntegerField):
+                        field_type = 2
+                    elif isinstance(field, models.FloatField):
+                        field_type = 3
+                    elif isinstance(field, models.ForeignKey):
+                        field_type = 4
+                        related_field = field.related_field
+                    elif isinstance(field, models.ManyToManyField):
+                        field_type = 5
+                    else:
+                        field_type = 0
+                except Exception, e:
+                    logger.log_errmsg("Field error: %s" % e)
 
-            field_types.append(field_type)
-            related_fields.append(related_field)
+                field_types.append(field_type)
+                related_fields.append(related_field)
 
-        # import values
-        # read next line
-        values = reader.next()
-        while values:
-            try:
-                record = {}
-                for item in zip(title, field_types, values, related_fields):
-                    field_name = item[0]
-                    field_type = item[1]
-                    value = item[2].decode(settings.WORLD_DATA_FILE_ENCODING)
-                    related_field = item[3]
-
-                    try:
-                        # set field values
-                        if field_type == 0:
-                            # default
-                            record[field_name] = value
-                        elif field_type == 1:
-                            # boolean value
-                            if value:
-                                record[field_name] = (int(value) != 0)
-                        elif field_type == 2:
-                            # interger value
-                            if value:
-                                record[field_name] = int(value)
-                        elif field_type == 3:
-                            # float value
-                            if value:
-                                record[field_name] = float(value)
-                        elif field_type == 4:
-                            # foreignKey
-                            if value:
-                                arg = {}
-                                arg[related_field.name] = value
-                                record[field_name] = related_field.model.objects.get(**arg)
-                    except Exception, e:
-                        print "value error: %s - '%s'" % (field_name, value)
-
-                # create new record
-                data = model_obj.objects.create(**record)
-                data.save()
-            except Exception, e:
-                print "Can't load %s %s: %s" % (file_name, values, e)
-
+            # import values
             # read next line
             values = reader.next()
 
-    except StopIteration:
-        # reach the end of file, pass this exception
-        pass
+            while values:
+                try:
+                    record = {}
+                    for item in zip(title, field_types, values, related_fields):
+                        field_name = item[0]
+                        field_type = item[1]
+                        if hasattr(item[2], "decode"):
+                            value = item[2].decode(settings.WORLD_DATA_FILE_ENCODING)
+                        else:
+                            value = item[2]
+                        related_field = item[3]
+
+                        try:
+                            # set field values
+                            if field_type == 0:
+                                # default
+                                record[field_name] = value
+                            elif field_type == 1:
+                                # boolean value
+                                if value:
+                                    record[field_name] = (int(value) != 0)
+                            elif field_type == 2:
+                                # interger value
+                                if value:
+                                    record[field_name] = int(value)
+                            elif field_type == 3:
+                                # float value
+                                if value:
+                                    record[field_name] = float(value)
+                            elif field_type == 4:
+                                # foreignKey
+                                if value:
+                                    arg = {}
+                                    arg[related_field.name] = value
+                                    record[field_name] = related_field.model.objects.get(**arg)
+                        except Exception, e:
+                            print "value error: %s - '%s'" % (field_name, value)
+
+                    # create new record
+                    data = model_obj.objects.create(**record)
+                    data.save()
+                except Exception, e:
+                    print "Can't load %s %s: %s" % (file_name, values, e)
+
+                # read next line
+                values = reader.next()
+
+        except StopIteration:
+            # reach the end of file, pass this exception
+            pass
+
+        break
+
+    if imported:
+        print "%s imported." % file_name
+    else:
+        print "Does not support file %s" % file_name
 
 
-def import_file(model_name, clear=True):
+def import_model(model_name, clear=True):
     """
     Import data from a data file to the db model
 
     Args:
-        model_name: (string) Db model's name.
-        clear: (boolean) clear old data or not.
+    model_name: (string) db model's name.
+    clear: (boolean) clear old data or not.
     """
-    file_search = os.path.join(settings.GAME_DIR, settings.WORLD_DATA_FOLDER, model_name + ".*")
-    file_names = glob.glob(file_search)
-
-    for file_name in file_names:
-        full_name = os.path.join(settings.GAME_DIR, settings.WORLD_DATA_FOLDER, file_name)
-
-        # get file's extension name
-        ext_name = os.path.splitext(file_name)[1].lower()
-
-        if ext_name == ".csv":
-            import_csv(full_name, model_name, clear)
-        else:
-            message = "Does not support file type %s" % ext_name[1:]
-            raise MudderyError(message)
+    file_name = os.path.join(settings.GAME_DIR, settings.WORLD_DATA_FOLDER, model_name)
+    import_file(file_name, model_name, widecard=True, clear=clear)
 
 
 def import_localized_strings(language=None):
@@ -175,18 +194,7 @@ def import_localized_strings(language=None):
                 # if it is a folder
                 continue
 
-            # get file's extension name
-            ext_name = os.path.splitext(file_name)[1].lower()
-
-            if ext_name == ".csv":
-                import_csv(full_name, settings.LOCALIZED_STRINGS_MODEL, True)
-            else:
-                message = "Does not support file type %s" % ext_name[1:]
-                raise MudderyError(message)
-                continue
-
-            ostring = "%s imported" % file_name
-            print ostring
+            import_file(full_name, settings.LOCALIZED_STRINGS_MODEL, widecard=False, clear=True)
 
 
 def import_all():
@@ -205,32 +213,7 @@ def import_all():
 
     # import models one by one
     for model_name in model_name_list:
-        # make file name
-        file_search = os.path.join(settings.GAME_DIR, settings.WORLD_DATA_FOLDER, model_name + ".*")
-        file_names = glob.glob(file_search)
-
-        for file_name in file_names:
-            full_name = os.path.join(settings.GAME_DIR, settings.WORLD_DATA_FOLDER, file_name)
-
-            # get file's extension name
-            ext_name = os.path.splitext(file_name)[1].lower()
-
-            # import data
-            try:
-                if ext_name == ".csv":
-                    import_csv(full_name, model_name)
-                else:
-                    message = "Does not support file type %s" % ext_name[1:]
-                    raise MudderyError(message)
-
-                ostring = "%s imported" % model_name
-                print ostring
-
-                count += 1
-            except Exception, e:
-                ostring = "Can not import %s: %s" % (model_name, e)
-                print ostring
-                continue
+        import_model(model_name)
 
     # import localized strings
     import_localized_strings(settings.LANGUAGE_CODE)
---
+++ muddery/utils/readers.py
@@ -0,0 +1,46 @@
+"""
+This module parse data files to lines.
+"""
+
+import csv
+
+try:
+    import xlrd
+except ImportError:
+    xlrd = None
+
+
+def csv_reader(file_name):
+    """
+    Parse csv files.
+
+    Args:
+        file_name: (string) csv file's name.
+    """
+    # load file
+    csvfile = open(file_name, 'r')
+    reader = csv.reader(csvfile)
+
+    # read lines
+    while True:
+        yield reader.next()
+
+
+def xls_reader(file_name):
+    """
+    Parse xls files.
+
+    Args:
+        file_name: (string) xls file's name.
+    """
+    if not xlrd:
+        print "You need to install 'xlrd' first to import xls/xlsx files."
+        return
+
+    # load file
+    data = xlrd.open_workbook(file_name)
+    table = data.sheet_by_index(0)
+
+    # read lines
+    for i in range(table.nrows):
+        yield table.row_values(i)
