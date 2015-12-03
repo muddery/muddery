871ac0930574ff4fb37d1658563c6b9bcad4a416
luyijun <luyijun999@gmail.com>
2015-12-03 17:02:59 +0800

    Add the giving object action.

--- muddery/utils/script_actions.py
+++ muddery/utils/script_actions.py
@@ -29,3 +29,25 @@ def learn_skill(character, obj, *args):
         return
 
     character.skill.learn_skill(args[0])
+
+
+def give_object(character, obj, *args):
+    """
+    Teach the character a skill.
+    args: object's key and object's number
+    """
+    if not character:
+        return
+
+    if not args:
+        return
+
+    obj_key = args[0]
+    number = 1
+    if len(args) > 1:
+        number = args[1]
+
+    obj_list = [{"object": obj_key,
+                 "number": number}]
+
+    character.receive_objects(obj_list)
