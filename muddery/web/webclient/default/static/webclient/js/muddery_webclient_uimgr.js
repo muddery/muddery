92f9996ccc35e2e3e33e2bd74639326de4a365ef
noahzao <234082230@qq.com>
2015-12-11 20:30:48 +0800

    modified skills and quests to jQuery

--- muddery/web/webclient/default/static/webclient/js/muddery_webclient_uimgr.js
+++ muddery/web/webclient/default/static/webclient/js/muddery_webclient_uimgr.js
@@ -16,12 +16,14 @@ var uimgr = {
             return divEmptyElement;
         }
     },
+
     divBR : function() {
         var divElement = uimgr.divEmpty();
         var BRElement = $("<br>")
             .appendTo(divElement);
         return divElement;
     },
+
     aHref : function(href, onclick, name, args) {
         var aHrefElement = $("<a>")
             .attr("href", href)
@@ -32,6 +34,7 @@ var uimgr = {
         }
         return aHrefElement;
     },
+
     divRoomTabName : function(element) {
         var divRoomTabNameElement = uimgr.divEmpty();
         var roomTabNameElement = $("<span>").text("\>\>\>\>\> " + element +  " \<\<\<\<\<")
@@ -39,6 +42,7 @@ var uimgr = {
             .appendTo(divRoomTabNameElement);
         return divRoomTabNameElement;
     },
+
     divRoomCenterTabName : function(element) {
         var divRoomTabNameElement = uimgr.divEmpty();
         var centerElement = $("<center>");
@@ -48,12 +52,14 @@ var uimgr = {
         centerElement.appendTo(divRoomTabNameElement);
         return divRoomTabNameElement;
     },
+
     divRoomCommon : function(room_id, room_title) {
         var divRoomCommonElement = uimgr.divEmpty()
             .attr("id", room_id)
             .text(room_title);
         return divRoomCommonElement;
     },
+
     divRoomCmds : function(data_cmds) {
         var divRoomCmdsElement = uimgr.divRoomCommon("room_cmds", "Commands:");
         for (var i in data_cmds) {
@@ -68,6 +74,7 @@ var uimgr = {
         }
         return divRoomCmdsElement;
     },
+
     divRoomExits : function(data_exits) {
         var empty = true;
         var divRoomExitsElement = uimgr.divRoomCommon("room_exits", LS("Exits:"));
@@ -90,6 +97,7 @@ var uimgr = {
         }
         return divRoomExitsElement;
     },
+
     divRoomThings : function(data_things) {
         var empty = true;
         var divRoomThingsElement = uimgr.divRoomCommon("room_things", LS("Objects:"));
@@ -110,6 +118,7 @@ var uimgr = {
         }
         return divRoomThingsElement;
     },
+
     divRoomNpcs : function(data_npcs) {
         var empty = true;
         var divRoomNpcsElement = uimgr.divRoomCommon("room_npcs", LS("NPCs:"));
@@ -120,10 +129,10 @@ var uimgr = {
                 var aHrefElement = uimgr.aHref("#", uimgr.CONST_A_HREF_ONCLICK, npc["name"],
                     {"cmd_name": "look", "cmd_args": npc["dbref"], "dbref": npc["dbref"], "style":"margin-left:10px;"});
                 if (npc["finish_quest"]) {
-                    aHrefElement.text(aHrefElement.text() + "[!]");
+                    aHrefElement.text(aHrefElement.text() + "[?]");
                 }
                 else if (npc["provide_quest"]) {
-                    aHrefElement.text(aHrefElement.text() + "[?]");
+                    aHrefElement.text(aHrefElement.text() + "[!]");
                 }
                 aHrefElement.appendTo(divRoomNpcsElement);
                 empty = false;
@@ -137,6 +146,7 @@ var uimgr = {
         }
         return divRoomNpcsElement;
     },
+
     divRoomPlayers : function(data_players) {
         var empty = true;
         var divRoomPlayersElement = uimgr.divRoomCommon("room_players", LS("Players:"));
@@ -157,12 +167,14 @@ var uimgr = {
         }
         return divRoomPlayersElement;
     },
+
     divObjectCommon : function(obj_id, obj_title) {
         var divObjectCommonElement = uimgr.divEmpty()
             .attr("id", obj_id)
             .text(obj_title);
         return divObjectCommonElement;
     },
+
     divObjectCmds : function(data_cmds) {
         var divObjectCmdsElement = uimgr.divObjectCommon("object_cmds", LS("Actions:"));
         // add cmds
@@ -178,6 +190,7 @@ var uimgr = {
         }
         return divObjectCmdsElement;
     },
+
     tableInventory : function(data) {
         var tableInventoryElement = $("<table>")
             .attr("class", "tab_inventory");
@@ -214,5 +227,85 @@ var uimgr = {
             }
         }
         return tableInventoryElement;
+    },
+
+    tableSkills : function(data) {
+        var tableSkillsElement = $("<table>")
+            .attr("class", "tab_skills");
+        var tableHeadElement = $("<thead>").appendTo(tableSkillsElement);
+        var tableHeadTRElement = $("<tr>").appendTo(tableHeadElement)
+            .append($("<th>").text(LS("NAME")))
+            .append($("<th>").text(LS("DESC")));
+
+        for (var i in data) {
+            try {
+                var obj = data[i];
+                var tbodyElement = $("<tbody>").appendTo(tableSkillsElement);
+                var trElement = $("<tr>").appendTo(tbodyElement);
+                var tdElement = $("<td>").appendTo(trElement);
+                var aHrefElement = $("<a>").appendTo(tdElement)
+                    .attr("href", "#")
+                    .attr("onclick", "commands.doCommandLink(this); return false;")
+                    .attr("cmd_name", "look")
+                    .attr("cmd_args", obj["dbref"])
+                    .text(obj["name"]);
+
+                tdElement = $("<td>").appendTo(trElement)
+                    .append(obj["desc"]);
+            }
+            catch(error) {
+            }
+        }
+        return tableSkillsElement;
+    },
+
+    tableQuests : function(data) {
+        var tableQuestsElement = $("<table>")
+            .attr("class", "tab_quests");
+        var tableHeadElement = $("<thead>").appendTo(tableQuestsElement);
+        var tableHeadTRElement = $("<tr>").appendTo(tableHeadElement)
+            .append($("<th>").text(LS("NAME")))
+            .append($("<th>").text(LS("DESC")))
+            .append($("<th>").text(LS("OBJECTIVE")));
+
+
+        console.log(JSON.stringify(data))
+
+        for (var i in data) {
+            try {
+                var quest = data[i];
+                var tbodyElement = $("<tbody>").appendTo(tableQuestsElement);
+                var trElement = $("<tr>").appendTo(tbodyElement);
+                var tdElement = $("<td>").appendTo(trElement);
+                var aHrefElement = $("<a>").appendTo(tdElement)
+                    .attr("href", "#")
+                    .attr("onclick", "commands.doCommandLink(this); return false;")
+                    .attr("cmd_name", "look")
+                    .attr("cmd_args", quest["dbref"])
+                    .text(quest["name"]);
+
+                tdElement = $("<td>").appendTo(trElement)
+                    .append(quest["desc"]);
+
+                tdElement = $("<td>").appendTo(trElement);
+                for (var o in quest["objectives"]) {
+                    if (tdElement.text() != "") {
+                        tdElement.append($("<br>"));
+                    }
+
+                    var obj = quest["objectives"][o];
+                    if ("desc" in obj) {
+                        tdElement.append(obj.desc);
+                    }
+                    else {
+                        tdElement.append(obj.target + " " + obj.object);
+                        tdElement.append(" " + obj.achieved + "/" + obj.total);
+                    }
+                }
+            }
+            catch(error) {
+            }
+        }
+        return tableQuestsElement;
     },
 }
