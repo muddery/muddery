351172ce7c71a15a8977669d65af4d5d03ef98e1
luyijun <luyijun999@gmail.com>
2015-12-27 00:27:41 +0800

    Add custom combat handler.

---
+++ muddery/game_template/typeclasses/combat_handler.py
@@ -0,0 +1,12 @@
+"""
+Combat handler.
+"""
+
+from muddery.typeclasses.combat_handler import MudderyCombatHandler
+
+
+class CombatHandler(MudderyCombatHandler):
+    """
+    This implements the combat handler.
+    """
+    pass
