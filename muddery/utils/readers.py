cbe805b006169d765a3889b478c26f42c1c3c4bb
luyijun <luyijun999@gmail.com>
2017-02-18 21:26:21 +0800

    Worldeditor/ Modify data readers.

--- muddery/utils/readers.py
+++ muddery/utils/readers.py
@@ -12,40 +12,107 @@ except ImportError:
     xlrd = None
 
 
-def csv_reader(file_name):
+class DataReader(object):
     """
-    Parse csv files.
-
-    Args:
-        file_name: (string) csv file's name.
+    Game data file reader.
     """
-    # load file
-    csvfile = open(file_name, 'r')
-    reader = csv.reader(csvfile)
+    def __init__(self, filename = None):
+        """
+        Args:
+            filename: (String) data file's name.
 
-    # read lines
-    while True:
-        yield reader.next()
+        Returns:
+            None
+        """
+        self.filename = filename
+
+    def readln(self):
+        """
+        Read data line.
+
+        Returns:
+            list: data line
+        """
+        # No data.
+        raise StopIteration
 
 
-def xls_reader(file_name):
+class CSVReader(DataReader):
     """
-    Parse xls files.
-
-    Args:
-        file_name: (string) xls file's name.
+    CSV file's reader.
     """
-    if not xlrd:
-        print('**********************************************************')
-        print('You need to install "xlrd" first to import xls/xlsx files!')
-        print('You can use "pip install xlrd" to install it!             ')
-        print('**********************************************************')
-        return
+    def __init__(self, filename=None):
+        """
+        Args:
+            filename: (String) data file's name.
 
-    # load file
-    data = xlrd.open_workbook(file_name)
-    table = data.sheet_by_index(0)
+        Returns:
+            None
+        """
+        super(CSVReader, self).__init__(filename)
 
-    # read lines
-    for i in range(table.nrows):
-        yield table.row_values(i)
+        self.reader = None
+        if filename:
+            csvfile = open(filename, 'r')
+            self.reader = csv.reader(csvfile)
+            print("self.reader: %s" % self.reader)
+
+    def readln(self):
+        """
+        Read data line.
+
+        Returns:
+            list: data line
+        """
+        if not self.reader:
+            raise StopIteration
+
+        # read line
+        return self.reader.next()
+
+
+class XLSReader(DataReader):
+    """
+    CSV file's reader.
+    """
+    def __init__(self, filename = None):
+        """
+        Args:
+            filename: (String) data file's name.
+
+        Returns:
+            None
+        """
+        super(XLSReader, self).__init__(filename)
+
+        if not xlrd:
+            print('**********************************************************')
+            print('You need to install "xlrd" first to import xls/xlsx files!')
+            print('You can use "pip install xlrd" to install it!             ')
+            print('**********************************************************')
+            return
+
+        # load file
+        self.table = None
+        self.row_pos = 0
+        if filename:
+            data = xlrd.open_workbook(filename)
+            self.table = data.sheet_by_index(0)
+
+    def readln(self):
+        """
+        Read data line.
+
+        Returns:
+            list: data line
+        """
+        if not self.table:
+            raise StopIteration
+
+        if self.row_pos >= self.table.nrows:
+            raise StopIteration
+
+        # read line
+        pos = self.row_pos
+        self.row_pos += 1
+        return self.table.row_values(pos)
