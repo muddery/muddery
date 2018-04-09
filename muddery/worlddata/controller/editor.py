e77e702a93f1d4bf7ebee8e566596725672fb68d
luyijun <luyijun999@gmail.com>
2018-04-09 09:51:29 +0800

    Add worlddata APIs.

--- muddery/worlddata/controller/editor.py
+++ muddery/worlddata/controller/editor.py
@@ -2,11 +2,14 @@
 Battle commands. They only can be used when a character is in a combat.
 """
 
+from __future__ import print_function
+
 from django.apps import apps
 from django.conf import settings
 from evennia.utils import logger
 from muddery.utils.localized_strings_handler import _
 from muddery.worlddata.processer import request_mapping
+from muddery.worlddata.type_field import TypeField
 
 
 @request_mapping
@@ -14,6 +17,15 @@ def query_type_tables(args):
     """
     Query all tables that contain custom types.
     """
-    worldata_app = apps.get_app_config(settings.WORLD_DATA_APP)
+    app = apps.get_app_config(settings.WORLD_DATA_APP)
+    models = {}
+    for model in app.get_models():
+        fields = []
+        for field in model._meta.get_fields():
+            if isinstance(field, TypeField):
+                fields.append(field.name)
+                
+        if fields:
+            models[model._meta.object_name] = fields;
 
-    return 0, "success"
+    return 0, models
---
+++ muddery/worlddata/controllers.py
@@ -0,0 +1,2 @@
+
+import muddery.worlddata.controller.editor
