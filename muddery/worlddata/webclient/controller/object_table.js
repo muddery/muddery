bd332ca24005c31e82966509f8cca87c37571068
luyijun <luyijun999@gmail.com>
2018-11-09 02:05:12 +0800

    Modify the typeclass data.

---
+++ muddery/worlddata/webclient/controller/object_table.js
@@ -0,0 +1,25 @@
+
+/*
+ * Derive from the base class.
+ */
+ObjectTable = function() {
+	CommonTable.call(this);
+}
+
+ObjectTable.prototype = prototype(CommonTable.prototype);
+ObjectTable.prototype.constructor = ObjectEditor;
+
+ObjectTable.prototype.onAddRecord: function(e) {
+    var editor = "object";
+    var table = controller.table_name;
+    window.parent.controller.editRecord(editor, table);
+}
+
+ObjectTable.prototype.onEdit: function(e) {
+    var object_key = $(this).attr("data-object-key");
+    if (object_key) {
+        var editor = "object";
+        var table = controller.table_name;
+        window.parent.controller.editObject(editor, typeclass, object_key);
+    }
+}
