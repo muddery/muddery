5185ff6544aa46a7b1cf9f628ee1c7678b88d6ed
luyijun <luyijun999@gmail.com>
2020-07-23 00:48:11 +0800

    Modify the event editor.

--- muddery/worldeditor/webclient/controller/dialogue_sentence_editor.js
+++ muddery/worldeditor/webclient/controller/dialogue_sentence_editor.js
@@ -5,7 +5,8 @@
 DialogueSentencesEditor = function() {
 	CommonEditor.call(this);
 
-    this.event_key = "";
+    this.event_fields = [];
+    this.event_table = "event_data";
 }
 
 DialogueSentencesEditor.prototype = prototype(CommonEditor.prototype);
@@ -14,6 +15,54 @@ DialogueSentencesEditor.prototype.constructor = DialogueSentencesEditor;
 DialogueSentencesEditor.prototype.init = function() {
     this.dialogue_key = utils.getQueryString("dialogue");
     CommonEditor.prototype.init.call(this);
+}
+
+/***********************************
+ *
+ * Events
+ *
+ ***********************************/
+DialogueSentencesEditor.prototype.bindEvents = function() {
+    CommonEditor.prototype.bindEvents.call(this);
+
+    $("#add-event").on("click", this.onAddEvent);
+    $("#event-table").on("click", ".edit-row", this.onEditEvent);
+    $("#event-table").on("click", ".delete-row", this.onDeleteEvent);
+}
+
+DialogueSentencesEditor.prototype.onEditEvent = function(e) {
+    var record_id = $(this).attr("data-record-id");
+    if (record_id) {
+        var editor = "event";
+        var args = {
+            trigger: controller.obj_key,
+             typeclass: controller.obj_typeclass,
+        }
+        window.parent.controller.editRecord(editor, controller.event_table, record_id, args);
+    }
+}
+
+DialogueSentencesEditor.prototype.onDeleteEvent = function(e) {
+    var record_id = $(this).attr("data-record-id");
+    window.parent.controller.confirm("",
+                                     "Delete this record?",
+                                     controller.confirmDeleteEvent,
+                                     {record: record_id});
+}
+
+DialogueSentencesEditor.prototype.confirmDeleteEvent = function(e) {
+    window.parent.controller.hideWaiting();
+
+    var record_id = e.data.record;
+    service.deleteRecord(controller.event_table, record_id, controller.deleteEventSuccess, controller.failedCallback);
+}
+
+DialogueSentencesEditor.prototype.deleteEventSuccess = function(data) {
+    var record_id = data.record;
+    $("#event-table").bootstrapTable("remove", {
+        field: "id",
+        values: [record_id],
+    });
 }
 
 DialogueSentencesEditor.prototype.setFields = function() {
