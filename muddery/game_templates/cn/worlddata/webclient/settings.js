a0aad69aa09add57b5baf39c87e547248a7d683a
luyijun <luyijun999@gmail.com>
2020-07-22 23:22:23 +0800

    Add settings model files of the webclient.

---
+++ muddery/game_templates/cn/worlddata/webclient/settings.js
@@ -0,0 +1,15 @@
+
+var settings = {{
+    // default values
+
+    // websocket's url
+    wsurl: {WEBSOCKET_HOST},
+
+    csessid: false,
+        
+    // resource's url
+    resource_url: {RESOURCE_HOST},
+
+    // default language
+    default_language: "zh-cn",
+}};
---
+++ muddery/game_templates/default/web/webclient_overrides/webclient/settings.js
@@ -0,0 +1,12 @@
+
+var settings = {{
+    // default values
+
+    // websocket's url
+    wsurl: {WEBSOCKET_HOST},
+
+    csessid: false,
+        
+    // resource's url
+    resource_url: {RESOURCE_HOST},
+}};
