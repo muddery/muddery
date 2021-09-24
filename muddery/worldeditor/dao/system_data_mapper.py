669fbbdb394d04995470e32e94f10d42f3387996
luyijun <luyijun999@gmail.com>
2021-09-25 00:06:35 +0800

    Use database's lock instead of processing's lock.

--- muddery/worldeditor/dao/system_data_mapper.py
+++ muddery/worldeditor/dao/system_data_mapper.py
@@ -2,50 +2,37 @@
 Query and deal common tables.
 """
 
-from multiprocessing import Lock
 from evennia.utils import logger
 from django.apps import apps
 from django.conf import settings
 from django.db import transaction
 
 
-def load_object_index():
-    model = apps.get_model(settings.WORLD_DATA_APP, "system_data")
-    if model.objects.count() == 0:
-        data = model()
-        data.full_clean()
-        data.save()
-
-    record = model.objects.first()
-    return record.object_index
-
-
 class SystemDataMapper(object):
     """
     The world editor system's data.
     """
-    __lock = Lock()
-    __object_index = load_object_index()
-
     def __init__(self):
         self.model_name = "system_data"
         self.model = apps.get_model(settings.WORLD_DATA_APP, self.model_name)
         self.objects = self.model.objects
 
+        if self.model.objects.count() == 0:
+            data = self.model()
+            data.full_clean()
+            data.save()
+
     def get_object_index(self):
         """
         Increase the object index and get the new value.
         """
-        self.__lock.acquire()
-        self.__object_index += 1
-        value = self.__object_index
-        self.__lock.release()
+        with transaction.atomic():
+            record = self.objects.select_for_update().first()
+            index = record.object_index
+            record.object_index = index + 1
+            record.save()
 
-        record = self.objects.first()
-        record.object_index = value
-        record.save()
-
-        return value
+        return index
 
 
 SYSTEM_DATA = SystemDataMapper()
